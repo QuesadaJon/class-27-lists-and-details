ce3f9827ebb56961aeca080f12357a9f99ad1848
Jonathan Quesada <jonnythan.quesada@gmail.com>
2021-02-09 22:55:30 -0800

    working router with functioning details page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import ListPage from './ListPage.jsx';
+import React, { Component } from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom';
+import DetailsPage from '../../containers/DetailsPage';
+import ListPage from '../../containers/ListPage';
 
-export default function App() {
-  return (
-    <>
-      <ListPage/>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route 
+              path="/details/:id"
+              exact
+              component={DetailsPage}
+            />
+            <Route 
+              path="/"
+              exact
+              component={ListPage}
+            /> 
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
